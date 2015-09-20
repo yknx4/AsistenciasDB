43c0c9cbddd0adb8749990e2457c5edc3021ce23
Jorge Figueroa <yknx4@outlook.com>
2015-09-20 17:36:09 -0500

    Fix DB URL

--- constants.js
+++ constants.js
@@ -4,8 +4,7 @@ module.exports = {
         var db_string = 'mongodb://yknx4:konami941994@ds059722.mongolab.com:59722/attendance';
         //take advantage of openshift env vars when available:
         if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-            //mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
-            db_string = 'mongodb://admin:_wZNQDlvA-EX@'+process.env.OPENSHIFT_MONGODB_DB_URL + 'attendance';
+            db_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'attendance';
         }
         return db_string;
 
