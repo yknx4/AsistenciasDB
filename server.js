7372f685bcca32b584240e9474564d6a0c40eb73
Jorge Figueroa <yknx.4.b@gmail.com>
2015-09-21 12:05:40 -0500

    Prob fix delete 4

--- server.js
+++ server.js
@@ -55,11 +55,12 @@ function unknownMethodHandler(req, res) {
     var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With']; // added Origin & X-Requested-With
 
     if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+    if (res.methods.indexOf('DELETE') === -1) res.methods.push('DELETE');
 
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
-    //res.header('Access-Control-Allow-Methods', res.methods.join(', '));
-    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+    //res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Origin', req.headers.origin);
 
     return res.send(204);
