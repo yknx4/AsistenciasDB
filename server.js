a8f19814474ef5a3c18b26de8ac0b74a0d1921f2
Jorge Figueroa <yknx.4.b@gmail.com>
2015-09-21 11:58:19 -0500

    Prob fix delete 2

--- server.js
+++ server.js
@@ -49,7 +49,25 @@ server.use(restify.CORS());
 server.use(restify.fullResponse());
 server.use(routes_helper.check_token);
 
+function unknownMethodHandler(req, res) {
+  if (req.method.toLowerCase() === 'options') {
+      console.log('received an options method request');
+    var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With']; // added Origin & X-Requested-With
 
+    if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+    res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+    return res.send(204);
+  }
+  else
+    return res.send(new restify.MethodNotAllowedError());
+}
+
+server.on('MethodNotAllowed', unknownMethodHandler);
 
 //server.get("/addcollection/:name", function (req, res) {
 //    if(typeof req.params.name === 'undefined'){
