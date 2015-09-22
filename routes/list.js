908b210763c6f29e9cb9031d11e645fbcf681372
Jorge Figueroa <yknx.4.b@gmail.com>
2015-09-22 00:43:37 -0500

    fix list

---
+++ routes/list.js
@@ -0,0 +1,70 @@
+///sales/:id/abonar/:cantidad//
+
+var defaults = require('../constants');
+var routes_helper = require('../routes_helper');
+var name = 'list';
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var crypto = require('crypto');
+var studentCollection = GLOBAL.db.collection('student');
+var attendanceCollection = GLOBAL.db.collection('attendance');
+var mongojs = require('mongojs');
+
+var route = {
+    route: '/' + name
+};
+
+route.protected = [];
+
+var getFN = function (req, res) {
+    var result = new Array();
+    var query = {
+        club_id: req.extras.club,
+        partial: parseInt(req.extras.partial),
+        student_id: 0
+    };
+    console.log("Getting list for :" + query.club_id + " (" + query.partial + ")");
+    // find the user
+    studentCollection.find({}, function (err, users) {
+            if (err) throw err;
+            users.forEach(function (user) {
+                //console.log(user);
+                query.student_id = user._id + '';
+                //console.log(JSON.stringify(query));
+                attendanceCollection.find(query, function (err, att) {
+
+                        if (err) throw err;
+                        if (att.length > 0) {
+                            console.log(att.length)
+                            user.attendances = att;
+                            result.push(user);
+                            console.log(result);
+                        }
+                        //if(att.length>0)console.log(att.length);
+                        if(users.indexOf(user) == (users.length-1)){
+                            res.send(result);
+                        }
+                    }
+
+                );
+
+            });
+
+            
+        }
+
+    );
+
+}
+
+route.get = function () {
+    var pre = [routes_helper.check_param_function('club'), routes_helper.check_param_function('partial')];
+    var mid = [];
+    if (route.protected.contains('get')) {
+        mid = mid.concat[routes_helper.enforce_login, routes_helper.check_master];
+    }
+    var fin = [getFN]
+    return pre.concat(mid).concat(fin);
+};
+
+
+module.exports = route;
